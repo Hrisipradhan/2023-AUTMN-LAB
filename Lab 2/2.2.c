ec311e4d90bec0f1cd91eda8820da596091f1b37
Hrisi Pradhan <84004287+Hrisipradhan@users.noreply.github.com>
2023-09-01 21:13:29 +0530

    Update 2.2.c

--- Lab 2/2.2.c
+++ Lab 2/2.2.c
@@ -2,10 +2,22 @@
 int main(int argc, char const *argv[])
 {
     int a=0;
-    int matrix[3][3] = { {1, 0, 2}, {3, 6, 8}, {3, 6, 8} };
+    // int matrix[3][3] = { {1, 0, 2}, {3, 6, 8}, {3, 6, 8} };
+    int matrix[30][30];
     int i, j;
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
+    int n;
+    printf("Enter size of square matrix:");
+    scanf("%d",&n);
+
+    printf("Enter elements fo the Matrix: ");
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
             printf("%d", matrix[i][j]);
             
             if (matrix[i][j]!=0){
@@ -20,10 +32,10 @@ int main(int argc, char const *argv[])
     printf("Non zero elements %d",a);
     printf("\n");
 
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
         if(i > j)
-            printf("0");
+            printf(" ");
         else
         printf("%d" , matrix[i][j]);
         printf(" ");  
