553cf6687b1d3deb8b9f6cd78fb0100e34e8245d
Hrisi Pradhan <84004287+Hrisipradhan@users.noreply.github.com>
2023-09-08 21:45:08 +0530

    Add files via upload

---
+++ Lab 2/1.7.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int r,c;
+    int a[10][20];
+    printf("Enter size of sparse Matrix:");
+    scanf("%d %d",&r,&c);
+    
+    printf("Enter the elements :");
+
+    for (int i=0; i < r; i++) {
+        for (int j = 0; j < c; j++ ) {
+            //printf("\nEnter the element of matrix[%d,%d] :",i+1,j+1);
+            scanf("%d",&a[i][j]);
+        }
+    }
+    
+    printf("Element of Matrix:\n");
+    for (int i=0; i < r; i++) {
+        for (int j = 0; j < c; j++ ) {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\nsparse matrix in 3-tuple format\n");
+    for (int i=0; i < r; i++) {
+        for (int j = 0; j < c; j++ ) {
+            if (a[i][j]!=0)
+            {
+                printf("%d %d %d\t",i,j,a[i][j]);
+                printf("\n");
+            }
+        }
+        
+    }
+
+
+    return 0;
+}
