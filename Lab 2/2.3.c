71200340d401a8e0c228f495d4148620537ae029
Hrisi Pradhan <84004287+Hrisipradhan@users.noreply.github.com>
2023-09-08 21:41:03 +0530

    2.1.c

--- Lab 2/2.3.c
+++ Lab 2/2.3.c
@@ -1,38 +1,114 @@
-#include<stdio.h>
-int main()
-{
-    int r,c;
-    int a[10][20];
-    printf("Enter size of sparse Matrix:");
-    scanf("%d %d",&r,&c);
-    
-    printf("       Enter the element     ");
+#include <stdio.h>
 
-    for (int i=0; i < r; i++) {
-        for (int j = 0; j < c; j++ ) {
-            printf("\nEnter the element of matrix[%d,%d] :",i+1,j+1);
-            scanf("%d",&a[i][j]);
-        }
-    }
-    
-    for (int i=0; i < r; i++) {
-        for (int j = 0; j < c; j++ ) {
-            printf("%d\t",a[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\nsparse matrix in 3-tuple format\n");
-    for (int i=0; i < r; i++) {
-        for (int j = 0; j < c; j++ ) {
-            if (a[i][j]!=0)
-            {
-                printf("%d %d %d\t",i,j,a[i][j]);
-                printf("\n");
-            }
-        }
-        
-    }
 
+void createArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        arr[i] = 0;
+    }
+}
+
+
+void displayArray(int arr[], int n) {
+    printf("Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+
+void insertElement(int arr[], int n, int element, int position) {
+    if (position < 0 || position > n) {
+        printf("Invalid position\n");
+        return;
+    }
+    for (int i = n - 1; i >= position; i--) {
+        arr[i + 1] = arr[i];
+    }
+    arr[position] = element;
+    printf("Inserted %d at position %d\n", element, position);
+}
+
+
+void deleteElement(int arr[], int n, int position) {
+    if (position < 0 || position >= n) {
+        printf("Invalid position\n");
+        return;
+    }
+    int deletedElement = arr[position];
+    for (int i = position; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    printf("Deleted %d from position %d\n", deletedElement, position);
+}
+
+
+int linearSearch(int arr[], int n, int element) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == element) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    createArray(arr, n);
+
+    // int element;
+    // int position;
+
+    while (1) {
+        printf("\nMenu:\n");
+        printf("a. Insert an element\n");
+        printf("b. Delete an element\n");
+        printf("c. Linear search\n");
+        printf("d. Display array\n");
+        printf("e. Exit\n");
+
+        char choice;
+        printf("Enter your choice: ");
+        scanf(" %c", &choice);
+
+        switch (choice) {
+            case 'a':;
+                int position;
+                int element;
+                printf("Enter the element to insert: ");
+                scanf("%d", &element);
+                printf("Enter the position to insert at: ");
+                scanf("%d", &position);
+                insertElement(arr, n, element, position);
+                break;
+            case 'b':
+                printf("Enter the position to delete from: ");
+                scanf("%d", &position);
+                deleteElement(arr, n, position);
+                break;
+            case 'c':
+                printf("Enter the element to search: ");
+                scanf("%d", &element);
+                int index = linearSearch(arr, n, element);
+                if (index != -1) {
+                    printf("Element %d found at position %d\n", element, index);
+                } else {
+                    printf("Element %d not found in the array\n", element);
+                }
+                break;
+            case 'd':
+                displayArray(arr, n);
+                break;
+            case 'e':
+                return 0;
+            default:
+                printf("Invalid choice\n");
+        }
+    }
 
     return 0;
 }
