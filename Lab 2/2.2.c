380960b960d7bb76c32b4f3ad9780599291f9f10
Hrisi Pradhan <84004287+Hrisipradhan@users.noreply.github.com>
2023-09-09 13:42:53 +0530

    Update 2.2.c

--- Lab 2/2.2.c
+++ Lab 2/2.2.c
@@ -17,7 +17,7 @@ int main()
 
     for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
-            printf("%d", matrix[i][j]);
+            printf(" %d ", matrix[i][j]);
             
             if (matrix[i][j]!=0){
                 a++;
@@ -31,6 +31,8 @@ int main()
     printf("Non zero elements %d",a);
     printf("\n");
 
+    printf("Upper Triangular Matrix\n");
+    printf("\n");
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
         if(i > j)
@@ -42,10 +44,36 @@ int main()
         printf("\n");
     }
 
-
-
-        
+    printf("Lower Triangular Matrix\n");
+    printf("\n");
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+        if(i < j)
+            printf(" ");
+        else
+        printf("%d" , matrix[i][j]);
+        printf(" ");  
+        }
+        printf("\n");
+    }
+    // 
     
-
+    printf("Elements above the main diagonal:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("Elements below the main diagonal:\n");
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+   
+    
     return 0;
 }
